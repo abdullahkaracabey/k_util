18185672d62f491db3628c953f75f60e1dd1a708
Abdullah Karacabey <abdullahkaracabey@gmail.com>
2024-08-16 16:08:56 +0300

    api mixing updated

--- lib/mixing/api.dart
+++ lib/mixing/api.dart
@@ -1,25 +1,15 @@
-import 'dart:io';
-
 import 'package:dio/dio.dart';
-import 'package:flutter/foundation.dart';
-import 'package:http_parser/http_parser.dart';
-import 'package:k_util/managers/base_auth_manager.dart';
 import 'package:k_util/models/app_error.dart';
-import 'package:uuid/uuid.dart';
 
 mixin Api {
-  bool hasConnection = true;
-
   String get baseUrl;
-
-  BaseAuthManager? get authManager;
-
-  VoidCallback? unAuthorizedCallback;
 
   Dio get dio;
 
-  Future<dynamic> postRequest(String url, dynamic body,
-      {Map<String, dynamic>? headers}) async {
+  Future<dynamic> postRequest(String url,
+      {Map<String, dynamic>? headers,
+      Map<String, dynamic>? queryParameters,
+      dynamic body}) async {
     try {
       var currentUrl = "";
 
@@ -28,101 +18,18 @@ mixin Api {
       } else {
         currentUrl = '$baseUrl/$url';
       }
-
-      var dio = _getDio();
-
-      dio.options.headers[HttpHeaders.contentTypeHeader] =
-          "application/x-www-form-urlencoded";
 
       if (headers != null) {
         dio.options.headers.addAll(headers);
       }
 
-      var response = await dio.post(currentUrl, data: body);
+      var response = await dio.post(currentUrl,
+          data: body, queryParameters: queryParameters);
 
       return _handledResponse(response);
     } catch (e) {
-      if (e is DioException) {
-        _handledResponse(e.response!);
-      } else if (e is AppException) {
-        rethrow;
-      }
-
-      throw AppException.kUnknownError;
+      _throwError(e);
     }
-  }
-
-  // Future<Map<String, dynamic>> multipartRequest(String url, File file) async {
-  //   try {
-  //     var request =
-  //         await http.MultipartRequest("POST", Uri.parse("$_baseUrl/$url"));
-
-  //     request.files.add(http.MultipartFile(
-  //         "picture", file.readAsBytes().asStream(), file.lengthSync()));
-
-  //     var response = await request.send();
-
-  //     return Map<String, dynamic>();
-  //     // return _handledResponse(response);
-  //   } catch (e) {
-  //     throw AppException(message: e.toString());
-  //   }
-  // }
-
-  Future<Map<String, dynamic>?> formDataRequest(String url, File file) async {
-    try {
-      String fileName = file.path.split('/').last;
-
-      var splittedFileName = fileName.split(".");
-      var extension = splittedFileName.isNotEmpty ? splittedFileName.last : "";
-
-      FormData data = FormData.fromMap({
-        "image": await MultipartFile.fromFile(file.path,
-            filename: "${const Uuid().v4()}.$extension",
-            contentType: MediaType("image", "png")),
-      });
-      debugPrint("form data: $data");
-      Dio dio = _getDio();
-
-      dio.options.connectTimeout = const Duration(seconds: 5);
-      dio.options.receiveTimeout = const Duration(seconds: 10);
-      dio.options.sendTimeout = const Duration(seconds: 10);
-
-      var currentUrl = "";
-
-      if (url.startsWith("http") || url.startsWith("www")) {
-        currentUrl = url;
-      } else {
-        currentUrl = '$baseUrl/$url';
-      }
-
-      var response = await dio.post(currentUrl, data: data);
-
-      return _handledResponse(response);
-    } catch (e) {
-      if (e is DioException) {
-        if (e.response != null) {
-          _handledResponse(e.response!);
-        } else {
-          int errorCode;
-
-          switch (e.type) {
-            case DioExceptionType.receiveTimeout:
-            case DioExceptionType.sendTimeout:
-              errorCode = AppException.kRemoteAddressNotReached;
-              break;
-            default:
-              errorCode = AppException.kUnknownError;
-          }
-
-          throw AppException(message: e.toString(), code: errorCode);
-        }
-
-        throw AppException(
-            message: e.toString(), code: AppException.kUnknownError);
-      }
-    }
-    return null;
   }
 
   Future<dynamic> getRequest(String url,
@@ -137,8 +44,6 @@ mixin Api {
         currentUrl = '$baseUrl/$url';
       }
 
-      var dio = _getDio();
-
       if (headers != null) {
         dio.options.headers.addAll(headers);
       }
@@ -148,14 +53,7 @@ mixin Api {
 
       return _handledResponse(response);
     } catch (e) {
-      if (e is DioException) {
-        if (e.response != null) {
-          _handledResponse(e.response!);
-          return;
-        }
-      }
-      throw AppException(
-          message: e.toString(), code: AppException.kUnknownError);
+      _throwError(e);
     }
   }
 
@@ -163,15 +61,11 @@ mixin Api {
     if (response.statusCode! ~/ 100 == 2) {
       return response.data;
     } else if (response.statusCode == 401) {
-      if (unAuthorizedCallback != null) {
-        unAuthorizedCallback!();
-      } else {
-        if (response.data["message"] != null) {
-          throw AppException(
-              code: response.statusCode, message: response.data["message"]);
-        }
-        throw AppException.unAuthorized;
+      if (response.data["message"] != null) {
+        throw AppException(
+            code: response.statusCode, message: response.data["message"]);
       }
+      throw AppException.unAuthorized;
     } else if (response.statusCode == 500) {
       throw AppException.unknownError;
     } else if (response.statusCode != null &&
@@ -185,23 +79,35 @@ mixin Api {
   }
 
   download({required String url, required String savePath}) async {
-    await _getDio().download(url, savePath);
+    try {
+      await dio.download(url, savePath);
+    } catch (e) {
+      _throwError(e);
+    }
   }
 
-  Dio _getDio() {
-    var dio = Dio();
-    dio.options.connectTimeout = const Duration(seconds: 5);
-    dio.options.receiveTimeout = const Duration(seconds: 10);
-    dio.options.sendTimeout = const Duration(seconds: 10);
-    var token = authManager?.authToken;
-    if (token != null) {
-      dio.options.headers[HttpHeaders.authorizationHeader] = "Bearer $token";
+  void _throwError(Object e) {
+    if (e is DioException) {
+      if (e.response != null) {
+        _handledResponse(e.response!);
+      } else {
+        int errorCode;
 
-      debugPrint("Token: $token");
+        switch (e.type) {
+          case DioExceptionType.connectionError:
+          case DioExceptionType.receiveTimeout:
+          case DioExceptionType.sendTimeout:
+            errorCode = AppException.kRemoteAddressNotReached;
+            break;
+          default:
+            errorCode = AppException.kUnknownError;
+        }
+
+        throw AppException(message: e.toString(), code: errorCode);
+      }
     } else {
-      debugPrint("Token not exist");
+      throw AppException(
+          message: e.toString(), code: AppException.kUnknownError);
     }
-
-    return dio;
   }
 }
