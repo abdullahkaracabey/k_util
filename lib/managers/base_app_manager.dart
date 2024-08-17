db12fb6788c723e439494cccfbb8014d90dc86b5
Abdullah Karacabey <abdullahkaracabey@gmail.com>
2024-08-17 16:09:27 +0300

    update for crashlytics

--- lib/managers/base_app_manager.dart
+++ lib/managers/base_app_manager.dart
@@ -1,9 +1,14 @@
+import 'dart:isolate';
+
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:k_util/managers/base_auth_manager.dart';
 import 'package:k_util/managers/firebase_notification_manager.dart';
+import 'package:k_util/models/app_error.dart';
 import 'package:universal_io/io.dart';
 
 Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
@@ -23,39 +28,39 @@ abstract class BaseAppManager<T> extends AsyncNotifier<T> {
       {required FireBaseBackgroundHandler onBackgroundMessage,
       required OnNotificationResponse onNotificationResponse,
       String? androidNotificationIconNativePath}) async {
-    // FlutterError.onError = (errorDetails) {
-    //   if (!FirebaseCrashlytics.instance.isCrashlyticsCollectionEnabled) {
-    //     return;
-    //   }
-    //   if (errorDetails.exception is AppException) {
-    //     return;
-    //   }
-    //   FirebaseCrashlytics.instance.recordFlutterError(errorDetails);
-    // };
-    // // Pass all uncaught asynchronous errors that aren't handled by the Flutter framework to Crashlytics
-    // PlatformDispatcher.instance.onError = (error, stack) {
-    //   if (!FirebaseCrashlytics.instance.isCrashlyticsCollectionEnabled) {
-    //     return false;
-    //   }
-    //   if (error is AppException == false) {
-    //     FirebaseCrashlytics.instance.recordError(
-    //       error,
-    //       stack,
-    //     );
-    //   }
-    //   return true;
-    // };
+    FlutterError.onError = (errorDetails) {
+      if (!FirebaseCrashlytics.instance.isCrashlyticsCollectionEnabled) {
+        return;
+      }
+      if (errorDetails.exception is AppException) {
+        return;
+      }
+      FirebaseCrashlytics.instance.recordFlutterError(errorDetails);
+    };
+    // Pass all uncaught asynchronous errors that aren't handled by the Flutter framework to Crashlytics
+    PlatformDispatcher.instance.onError = (error, stack) {
+      if (!FirebaseCrashlytics.instance.isCrashlyticsCollectionEnabled) {
+        return false;
+      }
+      if (error is AppException == false) {
+        FirebaseCrashlytics.instance.recordError(
+          error,
+          stack,
+        );
+      }
+      return true;
+    };
 
-    // Isolate.current.addErrorListener(RawReceivePort((pair) async {
-    //   if (!FirebaseCrashlytics.instance.isCrashlyticsCollectionEnabled) {
-    //     return;
-    //   }
-    //   final List<dynamic> errorAndStacktrace = pair;
-    //   await FirebaseCrashlytics.instance.recordError(
-    //     errorAndStacktrace.first,
-    //     errorAndStacktrace.last,
-    //   );
-    // }).sendPort);
+    Isolate.current.addErrorListener(RawReceivePort((pair) async {
+      if (!FirebaseCrashlytics.instance.isCrashlyticsCollectionEnabled) {
+        return;
+      }
+      final List<dynamic> errorAndStacktrace = pair;
+      await FirebaseCrashlytics.instance.recordError(
+        errorAndStacktrace.first,
+        errorAndStacktrace.last,
+      );
+    }).sendPort);
 
     await firebaseNotificationManager.initializeFireBaseMessaging(
         androidNotificationIconNativePath: androidNotificationIconNativePath,
