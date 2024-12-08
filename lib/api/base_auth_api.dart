1fe843379199daf7af7031b10525755ebe401f1d
Abdullah Karacabey <abdullahkaracabey@gmail.com>
2024-12-08 18:12:17 +0300

    updateUser method added

--- lib/api/base_auth_api.dart
+++ lib/api/base_auth_api.dart
@@ -3,4 +3,5 @@ import 'package:k_util/models/base_model.dart';
 abstract class BaseAuthApi<T extends BaseModel> {
   Future<void> logout();
   Future<T?> fetchUser();
+  Future<T> updateUser(T user);
 }
