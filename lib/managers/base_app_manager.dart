38d44cb7bade40d44d541b46ef6388c2594d22c0
Abdullah Karacabey <abdullahkaracabey@gmail.com>
2025-02-05 12:39:42 +0300

    Add app version update handling and integrate package_info_plus for version management

--- lib/managers/base_app_manager.dart
+++ lib/managers/base_app_manager.dart
@@ -1,3 +1,4 @@
+import 'package:app_version_update/app_version_update.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
@@ -7,6 +8,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:k_util/managers/base_auth_manager.dart';
 import 'package:k_util/managers/firebase_notification_manager.dart';
 import 'package:k_util/models/app_error.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 import 'package:universal_io/io.dart';
 
 Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
@@ -16,11 +18,24 @@ Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
   debugPrint('Handling a background message ${message.messageId}');
 }
 
-abstract class BaseAppManager<T> extends AsyncNotifier<T> {
+class BaseAppState {
+  final String? version;
+  final bool? needsUpdate;
+  final String? appName;
+
+  BaseAppState({this.appName, this.version, this.needsUpdate});
+
+  BaseAppState copyWith({String? appName, String? version, bool? needsUpdate}) {
+    return BaseAppState(
+        appName: appName ?? this.appName,
+        version: version ?? this.version,
+        needsUpdate: needsUpdate ?? this.needsUpdate);
+  }
+}
+
+abstract class BaseAppManager<T extends BaseAppState> extends AsyncNotifier<T> {
   BaseFirebaseNotificationManager get firebaseNotificationManager;
   BaseAuthManager? get authManager;
-
-  Future<String> appVersion();
 
   Future<void> initialize(
       {required FireBaseBackgroundHandler onBackgroundMessage,
@@ -68,6 +83,24 @@ abstract class BaseAppManager<T> extends AsyncNotifier<T> {
           onNotificationTokenUpdate: onNotificationTokenUpdate,
           onBackgroundMessage: _firebaseMessagingBackgroundHandler);
     }
+
+    PackageInfo packageInfo = await PackageInfo.fromPlatform();
+
+    if(kIsWeb){
+      await update((state) {
+        return state.copyWith(
+            version: packageInfo.version,
+            needsUpdate: false) as T;
+      });
+      return;
+    }
+    final updateAvailability = await AppVersionUpdate.checkForUpdates();
+
+    await update((state) {
+      return state.copyWith(
+          version: packageInfo.version,
+          needsUpdate: updateAvailability.canUpdate??false) as T;
+    });
   }
 
   Future<void> prepareAppAfterLogin();
